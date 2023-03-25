eff0e1f00a21f5da3f0d1e35911a7023aee94cde
Slim <slima4.u8@gmail.com>
2023-03-25 14:12:07 +0100

    implement Kafka producer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # collect-service
+
+GET http://localhost:8080/api/v1/total?year=2023&month=4&day=30
--- core/src/main/java/com/newyorktaxi/service/impl/KafkaProducerImpl.java
+++ core/src/main/java/com/newyorktaxi/service/impl/KafkaProducerImpl.java
@@ -6,11 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.function.BiConsumer;
 
 @Slf4j
 @Component
@@ -25,11 +28,24 @@ public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordB
         log.info("Sending message: {} to topic: {}", message, topicName);
 
         try {
-            kafkaTemplate.send(topicName, key, message);
+            kafkaTemplate.send(topicName, key, message)
+                    .whenComplete(callback());
         } catch (KafkaException e) {
             log.error("Error to send message: {} to topic: {} with exception: {}",
                     message, topicName, e.getMessage(), e);
             throw e;
         }
+    }
+
+    private BiConsumer<SendResult<K, V>, Throwable> callback() {
+        return (input, exception) -> {
+            if (exception != null) {
+                log.error("Error while sending message: {}", exception.getMessage(), exception);
+            } else {
+                final RecordMetadata metadata = input.getRecordMetadata();
+                log.info("Received new metadata. Topic: {}; Partition {}; Offset {}; Timestamp {}",
+                        metadata.topic(), metadata.partition(), metadata.offset(), metadata.timestamp());
+            }
+        };
     }
 }
